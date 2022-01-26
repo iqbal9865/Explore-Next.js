d78003bf8100bbdf21e4bdd06e2a6d93dcb1c9ba
Iqbal Ahmed <iqbalhussain9865@gmail.com>
2022-01-26 16:02:24 +0600

    file based nested routing

---
+++ pages/blogs/firstBlog.js
@@ -0,0 +1,8 @@
+const FirstBlog  = () => {
+    return (
+        <div>
+            <h1>1st Blogs Page</h1>
+        </div>
+    )
+}
+export default FirstBlog;
---
+++ pages/blogs/secondBlog.js
@@ -0,0 +1,8 @@
+const secondBlog  = () => {
+    return (
+        <div>
+            <h1>2nd Blogs Page</h1>
+        </div>
+    )
+}
+export default secondBlog;
